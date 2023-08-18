fffa5aa6dad9456e375a326b2a1eebdad9d69f9c
alok-urmaliya <alokurmaliya0123@gmail.com>
2023-08-19 01:08:50 +0530

    Set-up initial folder structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
-import logo from './logo.svg';
+import React from 'react';
+import Main from "./pages/_main/Main"
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to the project gamehub,  this is a demo for the pull request.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Main />
   );
 }
 
---
+++ src/pages/_main/Main.jsx
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react"
+import Carousel from "../../components/_carousel/Carousel"
+
+const Main = () => {
+    return (
+        <Fragment>
+            <Carousel />
+        </Fragment>
+    )
+}
+
+export default Main
