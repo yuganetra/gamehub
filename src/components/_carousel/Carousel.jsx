027b22a72406c510448f7b5e7541c138089451c9
alok-urmaliya <alokurmaliya0123@gmail.com>
2023-08-20 02:15:05 +0530

    WIP

--- src/components/_carousel/Carousel.jsx
+++ src/components/_carousel/Carousel.jsx
@@ -1,15 +1,27 @@
 import React from "react";
+import logo from '../../assets/icon/HatchfulExport-All/logo_transparent1.png';
 
 const Carousel = () => {
     return (
         <main>
-            <h1 className="logo">
-                Game
-                <span className="logo-2">
-                    Hub
-                </span>
-            </h1>
-            <p>carousel component is under development currently</p>
+            <div className="navbar">
+                <div className="logo">
+                    <img className="logo-image" src={logo} alt="logo" />
+                    <h1 className="logo-text">
+                        game
+                        <span>
+                            hub
+                        </span>
+                    </h1>
+                </div>
+
+                <ul className="nav-links">
+                    <li>Games</li>
+                    <li>About</li>
+                    <li>Team</li>
+                    <li>TEMP</li>
+                </ul>
+            </div>
         </main>
     )
 }
