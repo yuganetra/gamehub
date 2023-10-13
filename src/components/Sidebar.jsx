294226f22a2303e9fe5f309d855294a951591330
alok-urmaliya <alokurmaliya0123@gmail.com>
2023-10-14 00:23:01 +0530

    Fixed issue: #21

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,79 +1,73 @@
-import React from "react"; 
+import React from "react";
 import { PiGameControllerLight, PiGearLight } from "react-icons/pi";
 import { CiStreamOn } from "react-icons/ci";
 import { AiOutlineMessage, AiOutlineHome } from "react-icons/ai";
 import { SlPower } from "react-icons/sl";
-import {AiFillGitlab} from "react-icons/ai";
-// import { useNavigate } from "react-router-dom";
+import { AiFillGitlab } from "react-icons/ai";
+import { Link } from "react-router-dom";
 
 const Sidebar = () => {
-  // const navigate = useNavigate();
-
-  // const navigateToSignUp = ()=>{
-  //   navigate('/Signup');
-  // }
   return (
 
     <div className="sidebar">
       <ul className="menu-bar">
 
         <li title="Home" className="side-links home">
-          <a href="/" >
+          <Link to={"/"}>
             <span className="icons">
               <AiOutlineHome />
             </span>
-          </a>
+          </Link>
         </li>
 
         <li title="Profile" className="side-links home">
-          <a href="/Profile" >
+          <Link to={"/Profile"}>
             <span className="icons">
               <AiFillGitlab />
             </span>
-          </a>
+          </Link>
         </li>
 
         <li title="Games" className="side-links game">
-          <a href="/Games">
+          <Link to={"/Games"}>
             <span className="icons">
               <PiGameControllerLight />
             </span>
-          </a>
+          </Link>
         </li>
 
         <li title="Stream" className="side-links stream">
-          <a href="/Stream">
+          <Link to={"/Steam"}>
             <span className="icons">
               <CiStreamOn />
             </span>
-          </a>
+          </Link>
         </li>
 
         <li title="Messages" className="side-links message">
-          <a href="/Messages">
+          <Link to={"/Messages"}>
             <span className="icons">
               <AiOutlineMessage />
             </span>
-          </a>
+          </Link>
         </li>
 
         <li title="Setting" className="side-links setting">
-          <a href="/Setting">
+          <Link to={"/Settings"}>
             <span className="icons">
               <PiGearLight />
             </span>
-          </a>
+          </Link>
         </li>
-
       </ul>
 
       <ul className="login-container">
         <li title="Login" className="side-links login">
-        <a href="/Signup">
+          <Link to={"/Signup"}>
             <span className="icons">
               <SlPower />
             </span>
-          </a>
+          </Link>
         </li>
       </ul>
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import Main from '../components/Main';
-import Signup from '../components/Signup';
 import Profile from '../components/Profile';
+import Signup from "../components/Signup";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const Home = () => {
     return (
         <div className='Home'>
             <Main />
-           
         </div>
     );
 }
