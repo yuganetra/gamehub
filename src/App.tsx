8e851ae3d6fabdf9e2d957643c307571caac6468
alok-urmaliya <alokurmaliya0123@gmail.com>
2023-11-28 00:04:52 +0530

    Migrated from javascript to typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import Home from "./pages/Home";
+import Sidebar from "./components/Sidebar";
+import Signup from "./components/Signup";
+import { Routes, Route } from "react-router-dom";
+import Profile from "./components/Profile";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // gate click handling section
+  const [click, setClick] = useState(false);
+  const ClickHandler = () => {
+    setClick(true);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className="App">
+      {/* gate code */}
+      <div className={`gate-container ${click ? "gate-opened" : ""}`}>
+        <button onClick={ClickHandler} className="glowing-btn">
+          <span className="glowing-txt">
+            E<span className="faulty-letter">X</span>PLORE
+          </span>
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <Sidebar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Signup" element={<Signup />} />
+        <Route path="/Profile" element={<Profile />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import Main from '../components/Main';
+
+const Home = () => {
+    return (
+        <div className='Home'>
+            <Main />
+        </div>
+    );
+}
+
+export default Home;
